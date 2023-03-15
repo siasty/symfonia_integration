7488b2a6cd3a7857a0b9bcec3a0f3304ccf1c8e4
siasty3 <siasty3@gmail.com>
2023-03-15 11:42:40 +0000

    .

--- symfonia_integration/symfonia_integration/Customer/main.py
+++ symfonia_integration/symfonia_integration/Customer/main.py
@@ -2,10 +2,14 @@ from __future__ import unicode_literals
 import requests
 import frappe
 import json
+import os
 
 
 def addCustomer(customer, state):
-  with open('globals.json') as f:
-    content = json.load(f)
+  # Get the current working directory
+  cwd = os.getcwd()
 
-  frappe.msgprint(content)
+#  with open('globals.json') as f:
+#    content = json.load(f)
+
+  frappe.msgprint(cwd)
