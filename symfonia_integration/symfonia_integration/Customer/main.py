e8c79ad828d45eaa635ed24c5e53bdcfbbc549b1
siasty3 <siasty3@gmail.com>
2023-03-16 14:44:37 +0000

    .

--- symfonia_integration/symfonia_integration/Customer/main.py
+++ symfonia_integration/symfonia_integration/Customer/main.py
@@ -6,41 +6,4 @@ import os
 
 
 def addCustomer(customer, state):
-    # api_url = "https://jsonplaceholder.typicode.com/todos"
-    # todo = {
-    #     "Active": true,
-    #     "Code": customer.name,
-    #     "Name": customer.name,
-    #     "Vies": false,
-    #     "VATTaxPayer": true,
-    #     "SplitPayment": 0,
-    #     "NIP": customer.tax_id,
-    #     "Regon": "140045209",
-    #     "Pesel": "",
-    #     "CreditLimit": true,
-    #     "MaxCreditValue": 10.0,
-    #     "CreditCurrency": "PLN",
-    #     "Type": 0,
-    #     "Contact": {
-    #         "Name": "",
-    #         "Surname": "",
-    #         "Phone1": "22 455 56 00",
-    #         "Phone2": "",
-    #         "Fax": "22 455 57 00",
-    #         "Telex": "",
-    #         "Email": "kontakt@sage.com.pl",
-    #         "WWW": "www.sage.com.pl",
-    #         "Facebook": ""
-    #     },
-    #     "Address": {
-    #         "Country": "NL",
-    #         "City": "Warszawa",
-    #         "Province": "",
-    #         "Street": "J. Bema",
-    #         "HouseNo": "89",
-    #         "ApartmentNo": "",
-    #         "PostCode": "01-233"
-    #     },
-    #     "BankInfo": null
-    # }
-    frappe.msgprint(repr(customer))
+  frappe.msgprint(repr(customer))
