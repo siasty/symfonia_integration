f0edac935e991b2cfb3526c2a7a89b643cc7f023
siasty3 <siasty3@gmail.com>
2023-03-19 22:06:37 +0100

    .

--- install/after_install.py
+++ install/after_install.py
@@ -3,9 +3,15 @@ from frappe import _
 from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
 def install():
-  create_custom_field("Customer", {
-		"label": _("Regon"),
-		"fieldname": "regon",
-		"fieldtype": "Data",
-		"insert_after": "tax_id"
-		})
+    custom_fields = {
+        "Customer": [
+            {
+                "label": "Regon",
+                "fieldname": "regon",
+                "fieldtype": "Data",
+	            "insert_after": "tax_id"
+            }
+        ]
+    }
+    create_custom_fields(custom_fields)
+
