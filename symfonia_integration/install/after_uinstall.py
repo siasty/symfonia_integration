7cde0ad107cd85641c09d90c6d402eda43275a26
siasty3 <siasty3@gmail.com>
2023-03-20 21:09:17 +0100

    .

--- symfonia_integration/install/after_uinstall.py
+++ symfonia_integration/install/after_uinstall.py
@@ -2,12 +2,7 @@ import frappe
 from frappe import _
 
 def uinstall():
-  #  frappe.db.delete("Customer", {"name": "regon"})
-  #  frappe.db.delete("Customer", {"name": "pesel"})
-  #  frappe.db.delete("Customer", {"name": "split_payment"})
     custom_fields = frappe.get_all("Custom Field", filters={"dt": "Customer"})
     for cf in custom_fields:
-        print(cf.name)
-    #frappe.delete_doc("Custom Field", cf.name)
-    frappe.delete_doc("Custom Field", "Customer-split_payment")
+        frappe.delete_doc("Custom Field", cf.name)
     frappe.db.commit()
