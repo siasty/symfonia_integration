b3850eead8a34127ad7c486da1a8e15df068cb5f
siasty3 <siasty3@gmail.com>
2023-03-29 17:11:51 +0200

    .

--- symfonia_integration/symfonia_integration/Customer/model.py
+++ symfonia_integration/symfonia_integration/Customer/model.py
@@ -9,10 +9,54 @@ class SymfoniaModules:
     FKF = 1
 
 
-SymfoniaModules = SymfoniaModules()
+# SymfoniaModules = SymfoniaModules()
 
 
 class SymfoniaContractorModel:
+    def __erpnext_model(self):
+        return {
+            "name": "",
+            "owner": "",
+            "creation": "",
+            "modified": "",
+            "modified_by": "",
+            "docstatus": 0,
+            "idx": 0,
+            "naming_series": "CUST-.YYYY.-",
+            "salutation": "",
+            "customer_name": "",
+            "customer_type": "",
+            "customer_group": "",
+            "territory": "",
+            "gender": "",
+            "default_price_list": "",
+            "default_bank_account": "",
+            "default_currency": "",
+            "is_internal_customer": 0,
+            "market_segment": "",
+            "industry": "",
+            "website": "",
+            "language": "",
+            "customer_details": "",
+            "customer_primary_contact": "",
+            "mobile_no": "",
+            "email_id": "",
+            "customer_primary_address": "",
+            "primary_address": "",
+            "tax_id": "",
+            "tax_category": "",
+            "default_commission_rate": 0.0,
+            "so_required": 0,
+            "dn_required": 0,
+            "is_frozen": 0,
+            "disabled": 0,
+            "doctype": "Customer",
+            "accounts": [],
+            "sales_team": [],
+            "credit_limits": [],
+            "companies": []
+        }
+
     def __HMF_Contractor_Model(self):
         return {
             "Id": 0,
@@ -136,6 +180,8 @@ class SymfoniaContractorModel:
                     "Regon": "Customer.regon",
                     "Pesel": "Customer.pesel"
                 }
+    def __get_erpnext_model(self):
+        return SymfoniaObjectConvert(self.__erpnext_model())
 
     def __get_symfonia_hmf_contractor_model(self):
         return SymfoniaObjectConvert(self.__HMF_Contractor_Model())
@@ -151,12 +197,8 @@ class SymfoniaContractorModel:
                 return self.__get_symfonia_hmf_contractor_model()
 
     def set_maping(self, customer, modyleType):
-        meta = get_meta("Customer")
-        my_list = []
-        for field in meta.fields:
-            my_list.append({field.fieldname: ""})
-        meta_json = json.dumps(my_list, indent=4)
-        mapper.add(SymfoniaObjectConvert(meta_json),
-               self.__get_contractor_model_by_type(modyleType),
-               fields_mapping=self.__get_symfonia_map_fields(modyleType))
+
+        mapper.add(self.__get_erpnext_model,
+                   self.__get_contractor_model_by_type(modyleType),
+                   fields_mapping=self.__get_symfonia_map_fields(modyleType))
         return mapper.map(customer, use_deepcopy=False)
